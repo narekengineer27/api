31c6d8c20ceb35a18b99ce90833eb23f33e9dc07
Narek <avanenarek@gmail.com>
2021-06-08 22:30:20 -0400

    latest code

---
+++ README.md
@@ -0,0 +1,24 @@
+# Coinigy API
+
+Here you will find issue tracking, docs, and code examples for all Coinigy developer APIs
+
+
+
+REST HTTP API docs located here: http://docs.coinigy.apiary.io/#reference
+
+WEBSOCKET API docs located here: Coming soon!
+
+WEBSOCKET SERVER INFO:
+
+HOST: sc-02.coinigy.com*
+
+PORT: 443
+
+SECURE: true
+
+
+WEBSOCKET CLIENTS:
+
+https://github.com/SocketCluster/client-drivers
+
+*If your client requires the full wss url for websockets, use: wss://sc-02.coinigy.com/socketcluster/
---
+++ ws_example.js
@@ -0,0 +1,64 @@
+
+//run "npm install socketcluster-client" in terminal before attempting to use
+var socketCluster = require('socketcluster-client');
+
+var api_credentials =
+{
+    "apiKey"    : "",
+    "apiSecret" : ""
+}
+
+var options = {
+    hostname  : "sc-02.coinigy.com",    
+    port      : "443",
+    secure    : "true"
+};
+
+console.log(options);
+var SCsocket = socketCluster.connect(options);
+
+
+SCsocket.on('connect', function (status) {
+    
+    console.log(status); 
+    
+    SCsocket.on('error', function (err) {
+        console.log(err);
+    });    
+    
+
+    SCsocket.emit("auth", api_credentials, function (err, token) {        
+        
+        if (!err && token) {            
+
+            var scChannel = SCsocket.subscribe("TRADE-OK--BTC--CNY");
+            console.log(scChannel);
+            scChannel.watch(function (data) {
+                console.log(data);
+            });      
+            
+            SCsocket.emit("exchanges", null, function (err, data) {
+                if (!err) {                  
+                    console.log(data);
+                } else {
+                    console.log(err)
+                }   
+            });
+            
+            
+            SCsocket.emit("channels", "OK", function (err, data) {
+                if (!err) {
+                    console.log(data);
+                } else {
+                    console.log(err)
+                }   
+            }); 
+        } else {
+            console.log(err)
+        }   
+    });   
+});
+
+
+
+
